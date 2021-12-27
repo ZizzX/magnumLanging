f79c3f954f2ac211cf1676fa37033aa8d909b4b1
Aziz Isapov <zizzxiii@gmail.com>
2021-12-27 18:23:31 +0600

    scripts refactor

--- src/js/script.js
+++ src/js/script.js
@@ -4,10 +4,10 @@ import changeLanguage from './moduls/changeLanguage.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 	timer();
+	
 	let curLang = localStorage.getItem('lang') ?? 'ru';
+	
 	localStorage.setItem('lang', curLang);
-	changeLanguage({
-									 curLang,
-								 });
+	changeLanguage({curLang});
 });
 
